ca3b11a90f4d734b479bc27d3ab2e5d435cafb44
David Maurer <davemaurermail@gmail.com>
2015-12-01 11:08:43 -0700

    map using premade functions, 02-map

--- demos/array-prototype-methods/02-map/script.js
+++ demos/array-prototype-methods/02-map/script.js
@@ -1,7 +1,12 @@
 // TODO: Create an array called `simplifiedPhotographs` that takes `photographs` and transforms each one into an object with only two properties: `caption` and `url`.
 
-var simplifiedPhotographs = photographs;
+var simplifiedPhotographs = photographs.map(function (photo) {
+    return { caption: photo.caption.text,
+             url: photo.images.low_resolution.url
+    };
+});
 
 // TODO: Create an array called `photographElements` that takes `simplifiedPhotographs` and transforms each one into a DOM element.
 
-var photographElements = simplifiedPhotographs;
+var photographElements = simplifiedPhotographs.map(createImageElementFromPhotograph);
+  //  Turn each photo into a DOM node
